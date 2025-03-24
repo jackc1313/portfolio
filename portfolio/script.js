d8d83c216f20aeb267652f4653fdefdf0b16b426
Jacopo <179255721+jackc1313@users.noreply.github.com>
2025-03-24 15:14:45 +0100

    add a new functionality: roman number to arab number cnoverter

--- portfolio/script.js
+++ portfolio/script.js
@@ -10,6 +10,12 @@ document.getElementById("spinButton").addEventListener("click", function() {
     document.getElementById("spinOutput").textContent = spinnedSentence;
 })
 
+document.getElementById("romanToArabButton").addEventListener("click", function() {
+  const inputString = document.getElementById("romanToConvert").value;
+  const arabNumber = romanToArab(inputString);
+  document.getElementById("romanToArabOutput").textContent = arabNumber;
+})
+
 
 function toCamelCase(str){
     let arr = [];
@@ -51,6 +57,66 @@ function toCamelCase(str){
     }
     let reverseString = newArray.join(' ');
     return reverseString;
+}
+
+function romanToArab (roman) {
+  const numArr = [];
+  for(let i = 0; i < roman.length; i++)
+  {
+      switch(roman[i])
+      {
+          case 'M': if(roman.charAt(i - 1) == 'C'){
+              numArr.push(900);
+          } else {
+              numArr.push(1000);
+          }
+          break;
+          case 'D': if(roman.charAt(i - 1) == 'C'){
+              numArr.push(400);
+          } else {
+              numArr.push(500);
+          }
+          break;
+          case 'C': if(roman.charAt(i + 1) == 'M' || roman.charAt(i + 1) == 'D') {
+              continue;
+          } else if(roman.charAt(i - 1) == 'X') {
+              numArr.push(90);
+          }else{
+              numArr.push(100);
+          }
+          break;
+          case 'L': if(roman.charAt(i - 1) == 'X'){
+              numArr.push(40);
+          } else {
+              numArr.push(50);
+          }
+          break;
+          case 'X': if(roman.charAt(i + 1) == 'C' || roman.charAt(i + 1) == 'L') {
+              continue;
+          } else if(roman.charAt(i - 1) == 'I') {
+              numArr.push(9);
+          }else{
+              numArr.push(10);
+          }
+          break;
+          case 'V': if(roman.charAt(i - 1) == 'I'){
+              numArr.push(4);
+          } else {
+              numArr.push(5);
+          }
+          break;
+          case 'I': if(roman.charAt(i + 1) == 'X' || roman.charAt(i + 1) == 'V') {
+              continue;
+          }else{
+              numArr.push(1);
+          }
+          break;
+      }
+  }
+  let number = numArr.reduce((accum, current) => {
+      return accum + current;
+  }, 0);
+  return number;
 }
 
   // Script to toggle sidebar on mobile
